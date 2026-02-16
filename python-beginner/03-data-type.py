4dc4aa19e22c17a7268ac3c5fad5bb7d4919c1fa
takayums <takayums20@gmail.com>
2026-02-16 09:44:20 +0700

    03-data-type

---
+++ python-beginner/03-data-type.py
@@ -0,0 +1,60 @@
+# Tipe Data
+# Text - str
+# Numerik - int, float, complex
+# Sequence - list, tuple, range
+# Mapping - dict
+# Set - set, frozenset
+# Boolean - bool
+# Binary - bytes, bytearray, memoryview
+# None - NoneType
+
+# str
+gretting = "Hello World"
+print(type(gretting))
+
+# float
+size = 15.5
+print(type(size))
+
+# int
+age = 20
+print(type(age))
+
+# complex
+cmplx = 1j
+print(type(cmplx))
+
+# list
+array = ["apple", "banana", "manggo"]
+print(type(array))
+
+# tupple
+tpl = ("asraf", "dika", "habib")
+print(type(tpl))
+
+# dict
+hero = {"power": 100, size: "big"}
+print(type(hero))
+
+# frozenset
+forz = frozenset({"apple", "banana", "apple"})
+print(type(forz))
+
+# bool
+answare = True
+print(type(answare))
+
+# byte
+say = b"hello"
+print(type(say))
+
+# bytearray
+array_byte = bytearray(5)
+print(array_byte)
+
+# memoryview
+memory = memoryview(bytes(5))
+
+# none
+none = None
+print(type(none))
