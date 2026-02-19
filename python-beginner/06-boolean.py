cf0f93be9e1b66ff36e39a4aebebb4b155ad7d98
takayums <takayums20@gmail.com>
2026-02-19 09:36:06 +0700

    06-boolean

---
+++ python-beginner/06-boolean.py
@@ -0,0 +1,27 @@
+# Python Boolean
+
+# Value Booelan is True or False
+
+print(8 > 10)
+print(10 == 10)
+
+# Evaluete Value dnd Variable
+print(bool("hello"))
+print(bool(0))
+
+# Values are True or False
+# String Kosong = False
+# Array, Tupple, and Dictinionery Kosong = False
+# Number 0 = False
+# None = False
+
+
+# Function Return Boolean
+def myFunction():
+    return True
+
+
+if myFunction():
+    print("True")
+else:
+    print("False")
